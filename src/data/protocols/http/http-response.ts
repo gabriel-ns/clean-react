3a6482d407185b035e1b90d40e6f4fdcba849178
Gabriel Santos <gaabrielns@gmail.com>
2022-02-05 20:41:57 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError on 500

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -2,7 +2,8 @@ export enum HttpStatusCode {
   ok = 200,
   noContent = 204,
   badRequest = 400,
-  unauthorized = 401
+  unauthorized = 401,
+  serverError = 500,
 }
 
 export type HttpResponse = {
