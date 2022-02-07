7df148261ad0ffd88bc5b8432864cf16148983e8
Gabriel Santos <gaabrielns@gmail.com>
2022-02-07 17:49:10 -0300

    refactor: move axios mock to a separate file

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,42 +1,36 @@
 import { AxiosHttpClient } from './axios-http-client'
+import { mockAxios } from '@/infra/http/test'
+import { mockPostRequest } from '@/data/test'
 import axios from 'axios'
-import faker from 'faker'
-import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
-const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxios = mockAxios()
 
-const mockedAxiosPostResponse = ({
-  data: faker.random.objectElement(),
-  status: faker.random.number()
-})
-
-mockedAxios.post.mockResolvedValue(mockedAxiosPostResponse)
-
-const makeSut = (): { sut: AxiosHttpClient } => {
-  const sut = new AxiosHttpClient()
-  return { sut }
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
 }
 
-const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.objectElement()
-})
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  return { sut, mockedAxios }
+}
 
 describe('AxiosHttpClient', () => {
   test('Should call Axios with correct values', async () => {
     const postRequest = mockPostRequest()
-    const { sut } = makeSut()
+    const { sut, mockedAxios } = makeSut()
     await sut.post(postRequest)
     expect(mockedAxios.post).toHaveBeenCalledWith(postRequest.url, postRequest.body)
   })
 
   test('Should return the correct status code and body', async () => {
-    const { sut } = makeSut()
+    const { sut, mockedAxios } = makeSut()
     const httpResponse = await sut.post(mockPostRequest())
-    expect(httpResponse).toEqual({
-      statusCode: mockedAxiosPostResponse.status,
-      body: mockedAxiosPostResponse.data
+    const axiosResponse = await mockedAxios.post.mock.results[0].value
+    await expect(httpResponse).toEqual({
+      body: axiosResponse.data,
+      statusCode: axiosResponse.status
     })
   })
 })
