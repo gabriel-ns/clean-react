c5781e38386947f65af13dfb142eb07a36c1854e
Gabriel Santos <gaabrielns@gmail.com>
2022-02-14 06:39:50 -0300

    test: ensure Login starts with input in the required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -14,4 +14,14 @@ describe('Login Component', () => {
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBeTruthy()
   })
+
+  test('Should disable submit button on start', () => {
+    const { getByTestId } = render(<Login />)
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo Obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo Obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
+  })
 })
