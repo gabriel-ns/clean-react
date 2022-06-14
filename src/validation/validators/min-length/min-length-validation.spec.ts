2a4b9be8b8744f82fd9d3347b58278b01f591460
Gabriel Santos <gaabrielns@gmail.com>
2022-06-14 19:10:26 -0300

    test: ensure MinLengthValidation returns falsy if value length is grater than validation length

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -8,9 +8,15 @@ describe('MinLengthValidation', () => {
     expect(error).toEqual(new InvalidFieldError())
   })
 
-  test('Should return falsy if value is valid', () => {
+  test('Should return falsy if value length equals validation length', () => {
     const sut = new MinLengthValidation('field', 5)
     const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
+
+  test('Should return falsy if value length is greater validation length', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('1234567')
     expect(error).toBeFalsy()
   })
 })
