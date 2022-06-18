2e1683244c7c075d66b783b99d42aece25a2d348
Gabriel Santos <gaabrielns@gmail.com>
2022-06-18 15:19:23 -0300

    feat: ensure RemoteAddAccount calls HttpPostClient with correct URL

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -7,6 +7,6 @@ export type AddAccountParams = {
   passwordConfirmation: string
 }
 
-export interface addAccount {
+export interface AddAccount {
   add: (params: AddAccountParams) => Promise<AccountModel>
 }
