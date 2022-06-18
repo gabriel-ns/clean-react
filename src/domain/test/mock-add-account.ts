5d8917ff56c8f8e9444492797fe81d5c516647d9
Gabriel Santos <gaabrielns@gmail.com>
2022-06-18 15:19:52 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,12 @@
+import { AddAccountParams } from '@/domain/usecases'
+import faker from 'faker'
+
+export const mockAddAccountParams = (): AddAccountParams => {
+  const password = faker.internet.password()
+  return {
+    email: faker.internet.email(),
+    name: faker.name.findName(),
+    password,
+    passwordConfirmation: password
+  }
+}
