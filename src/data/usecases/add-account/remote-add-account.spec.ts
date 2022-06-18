b8bbb7d2090a9244a8647e4b3e4225a1a9b36e22
Gabriel Santos <gaabrielns@gmail.com>
2022-06-18 15:22:51 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct body

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -27,4 +27,11 @@ describe('RemoteAddAccount', () => {
     await sut.add(mockAddAccountParams())
     expect(httpPostClientSpy.url).toBe(url)
   })
+
+  test('Should call HttpPostClient with correct body', async () => {
+    const { httpPostClientSpy, sut } = makeSut()
+    const addAccountParams = mockAddAccountParams()
+    await sut.add(addAccountParams)
+    expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
 })
