af1f5036eef3e9fdaa0593e9ad41ae0c07b47bc7
Gabriel Santos <gaabrielns@gmail.com>
2022-06-20 21:31:04 -0300

    test: ensure Signup srenders with submit button disabled on start

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -26,9 +26,19 @@ const testChildCount = (sut: RenderResult, fieldTestId: string, count: number): 
   expect(el.childElementCount).toBe(count)
 }
 
+const testButtonDisabled = (sut: RenderResult, fieldTestId: string, isDisabled: boolean): void => {
+  const button = sut.getByTestId(fieldTestId) as HTMLButtonElement
+  expect(button.disabled).toBe(isDisabled)
+}
+
 describe('Signup Component', () => {
   test('Should not render spinner and error on start', () => {
     const { sut } = makeSut()
     testChildCount(sut, 'error-wrap', 0)
+  })
+
+  test('Should disable submit button on start', () => {
+    const { sut } = makeSut()
+    testButtonDisabled(sut, 'submit', true)
   })
 })
