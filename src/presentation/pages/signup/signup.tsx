c325d7cee47fd61041019f8f8062eed4df0f9888
Gabriel Santos <gaabrielns@gmail.com>
2022-06-20 21:30:45 -0300

    feat: ensure Signup srenders with submit button disabled on start

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -15,7 +15,7 @@ const Signup: React.FC = () => {
           <Input type="email" name="email" placeholder="Digite seu email" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
-          <button className={Styles.submit} type="submit">Entrar</button>
+          <button data-testid='submit' className={Styles.submit} disabled type="submit">Entrar</button>
           <Link to="/login" className={Styles.link}>Voltar para o login</Link>
           <FormStatus />
         </form>
