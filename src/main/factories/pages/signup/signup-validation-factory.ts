9697e085d1ea382e4206ba490d9daa2b6b5e10a1
Gabriel Santos <gaabrielns@gmail.com>
2022-06-21 16:45:10 -0300

    feat: add SignupValidation factory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,12 @@
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+export const makeSignupValidation = (): ValidationComposite => {
+  const validationComposite = new ValidationComposite([
+    ...Builder.field('name').required().minLength(3).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().minLength(5).build(),
+    ...Builder.field('passwordConfirmation').required().minLength(5).build()
+  ])
+  return validationComposite
+}
