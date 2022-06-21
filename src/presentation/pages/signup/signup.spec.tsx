99ac76083fad77930d56cd9011f63f7dbc58444c
Gabriel Santos <gaabrielns@gmail.com>
2022-06-20 21:26:21 -0300

    test: ensure Signup renders without spinner and error on start

---
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -0,0 +1,34 @@
+import { render, RenderResult } from '@testing-library/react'
+import { createMemoryHistory, MemoryHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router-dom'
+import Signup from './signup'
+
+type SutTypes = {
+  sut: RenderResult
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/signup'] })
+  const sut = render(
+    <Router navigator={ history } location={ history.location }>
+      <Signup />
+    </Router>)
+  return {
+    sut,
+    history
+  }
+}
+
+const testChildCount = (sut: RenderResult, fieldTestId: string, count: number): void => {
+  const el = sut.getByTestId(fieldTestId)
+  expect(el.childElementCount).toBe(count)
+}
+
+describe('Signup Component', () => {
+  test('Should not render spinner and error on start', () => {
+    const { sut } = makeSut()
+    testChildCount(sut, 'error-wrap', 0)
+  })
+})
