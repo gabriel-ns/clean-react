a58eeb5de844181898175f719a872e0e1da53284
Gabriel Santos <gaabrielns@gmail.com>
2022-06-28 11:36:26 -0300

    feat: add SurveyList layout

---
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -0,0 +1,98 @@
+import { Footer, Logo } from '@/presentation/components'
+import React from 'react'
+import Styles from './survey-list-styles.scss'
+
+const SurveyList: React.FC = () => {
+  return (
+  <div className={ Styles.surveyListWrap }>
+    <header className={ Styles.headerWrap }>
+      <div className={ Styles.headerContent }>
+        <Logo />
+        <div className={Styles.logoutWrap}>
+          <span>Username</span>
+          <a href='#'>Logout</a>
+        </div>
+      </div>
+    </header>
+
+    <div className={Styles.contentWrap}>
+      <h2>Enquetes</h2>
+      <ul>
+        <li>
+          <div className={Styles.surveyContent}>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>12</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>
+              Lorem ipsum dolor sit amet?
+            </p>
+          </div>
+          <footer>Ver resultado</footer>
+        </li>
+
+        <li>
+          <div className={Styles.surveyContent}>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>12</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>
+              Lorem ipsum dolor sit amet?
+            </p>
+          </div>
+          <footer>Ver resultado</footer>
+        </li>
+
+        <li>
+          <div className={Styles.surveyContent}>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>12</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>
+              Lorem ipsum dolor sit amet?
+            </p>
+          </div>
+          <footer>Ver resultado</footer>
+        </li>
+
+        <li>
+          <div className={Styles.surveyContent}>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>12</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>
+              Lorem ipsum dolor sit amet?
+            </p>
+          </div>
+          <footer>Ver resultado</footer>
+        </li>
+
+        <li>
+          <div className={Styles.surveyContent}>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>12</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>
+              Lorem ipsum dolor sit amet?
+            </p>
+          </div>
+          <footer>Ver resultado</footer>
+        </li>
+      </ul>
+    </div>
+
+    <Footer />
+  </div>
+  )
+}
+
+export default SurveyList
