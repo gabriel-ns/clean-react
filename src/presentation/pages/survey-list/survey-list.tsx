edcd5c791c34f3b0607a11a636cdc250ba72e19d
Gabriel Santos <gaabrielns@gmail.com>
2022-06-28 15:09:53 -0300

    refactor: extract SurveyList item to a custom component

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -2,6 +2,25 @@ import { Footer, Header, Icon, IconName } from '@/presentation/components'
 import React from 'react'
 import Styles from './survey-list-styles.scss'
 
+const SurveyItem: React.FC = () => {
+  return (
+    <>
+    <div className={Styles.surveyContent}>
+      <Icon iconName={IconName.thumbDown} className={Styles.iconWrap}/>
+      <time>
+        <span className={Styles.day}>22</span>
+        <span className={Styles.month}>12</span>
+        <span className={Styles.year}>2022</span>
+      </time>
+      <p>
+        Lorem ipsum dolor sit amet?
+      </p>
+    </div>
+    <footer>Ver resultado</footer>
+  </>
+  )
+}
+
 const SurveyList: React.FC = () => {
   return (
   <div className={ Styles.surveyListWrap }>
@@ -10,76 +29,12 @@ const SurveyList: React.FC = () => {
     <div className={Styles.contentWrap}>
       <h2>Enquetes</h2>
       <ul>
-        <li>
-          <div className={Styles.surveyContent}>
-            <Icon iconName={IconName.thumbDown} className={Styles.iconWrap}/>
-            <time>
-              <span className={Styles.day}>22</span>
-              <span className={Styles.month}>12</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-            <p>
-              Lorem ipsum dolor sit amet?
-            </p>
-          </div>
-          <footer>Ver resultado</footer>
-        </li>
-
-        <li>
-          <div className={Styles.surveyContent}>
-            <time>
-              <span className={Styles.day}>22</span>
-              <span className={Styles.month}>12</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-            <p>
-              Lorem ipsum dolor sit amet?
-            </p>
-          </div>
-          <footer>Ver resultado</footer>
-        </li>
-
-        <li>
-          <div className={Styles.surveyContent}>
-            <time>
-              <span className={Styles.day}>22</span>
-              <span className={Styles.month}>12</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-            <p>
-              Lorem ipsum dolor sit amet?
-            </p>
-          </div>
-          <footer>Ver resultado</footer>
-        </li>
-
-        <li>
-          <div className={Styles.surveyContent}>
-            <time>
-              <span className={Styles.day}>22</span>
-              <span className={Styles.month}>12</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-            <p>
-              Lorem ipsum dolor sit amet?
-            </p>
-          </div>
-          <footer>Ver resultado</footer>
-        </li>
-
-        <li>
-          <div className={Styles.surveyContent}>
-            <time>
-              <span className={Styles.day}>22</span>
-              <span className={Styles.month}>12</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-            <p>
-              Lorem ipsum dolor sit amet?
-            </p>
-          </div>
-          <footer>Ver resultado</footer>
-        </li>
+        <li><SurveyItem /></li>
+        <li><SurveyItem /></li>
+        <li><SurveyItem /></li>
+        <li><SurveyItem /></li>
+        <li><SurveyItem /></li>
+        <li><SurveyItem /></li>
       </ul>
     </div>
 
