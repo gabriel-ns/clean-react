e34bc0cedfc2a9483df22ef01b48d643d8ed6ad6
Gabriel Santos <gaabrielns@gmail.com>
2022-06-29 10:26:57 -0300

    refactor: move Router to main layer

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,18 +1,15 @@
 import SurveyList from '@/presentation/pages/survey-list/survey-list'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { makeLogin as MakeLogin } from '@/main/factories/pages/login/login-factory'
+import { makeSignup as MakeSignup } from '@/main/factories/pages/signup/signup-factory'
 
-type Factory = {
-  MakeLogin: React.FC
-  MakeSignup: React.FC
-}
-
-const Router: React.FC<Factory> = (factory: Factory) => {
+const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<factory.MakeLogin />} />
-        <Route path="/signup" element={<factory.MakeSignup />} />
+        <Route path="/login" element={<MakeLogin />} />
+        <Route path="/signup" element={<MakeSignup />} />
         <Route path="/" element={<SurveyList />} />
       </Routes>
     </BrowserRouter>
