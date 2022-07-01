7c0244e89aba6ba6258010e5117dca5eabcda289
Gabriel Santos <gaabrielns@gmail.com>
2022-07-01 15:48:02 -0300

    refactor: move setCurrentAccountAdapter to its own file

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,16 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { makeLogin as MakeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignup as MakeSignup } from '@/main/factories/pages/signup/signup-factory'
 import { ApiContext } from '@/presentation/contexts'
-import { AccountModel } from '@/domain/models'
-import { UnexpectedError } from '@/domain/errors'
-import { makeLocalStorageAdapter } from '../factories/cache/local-storage-adapter-factory'
-
-const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account?.accessToken) {
-    throw new UnexpectedError()
-  }
-  void makeLocalStorageAdapter().set('account', account)
-}
+import { setCurrentAccountAdapter } from '../adapters/current-account-adapter'
 
 const Router: React.FC = () => {
   return (
