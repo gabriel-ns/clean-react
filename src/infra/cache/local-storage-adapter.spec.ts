5cf06a8d45c4adb4fc31ab1dfd126aacb9a67f75
Gabriel Santos <gaabrielns@gmail.com>
2022-07-01 16:30:08 -0300

    refactor: remove unecessary async/await

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -11,20 +11,20 @@ describe('LocalStorageAdapter', () => {
     localStorage.clear()
   })
 
-  test('Should call localStorage.setItem with correct values', async () => {
+  test('Should call localStorage.setItem with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.objectElement<AccountModel>()
-    await sut.set(key, value)
+    sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
   })
 
-  test('Should call localStorage.getItem with correct value', async () => {
+  test('Should call localStorage.getItem with correct value', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = mockAccountModel()
     jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
-    const account = await sut.get(key)
+    const account = sut.get(key)
     expect(localStorage.getItem).toHaveBeenCalledWith(key)
     expect(account).toEqual(value)
   })
