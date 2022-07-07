105c27cbe274e1ce89f65225af148933b641686d
Gabriel Santos <gaabrielns@gmail.com>
2022-07-07 11:56:36 -0300

    refactor: generate random day and year in date test

--- src/presentation/pages/survey-list/components/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item.spec.tsx
@@ -3,6 +3,7 @@ import { mockSurveyModel } from '@/domain/test'
 import { IconName } from '@/presentation/components'
 import { SurveyItem } from '@/presentation/pages/survey-list/components'
 import { render, screen } from '@testing-library/react'
+import faker from 'faker'
 
 const makeSut = (survey = mockSurveyModel()): void => {
   render(<SurveyItem survey={ survey }/>)
@@ -31,10 +32,12 @@ describe('SurveyItem component', () => {
 
   test('Should render with correct date', () => {
     const survey = mockSurveyModel()
-    survey.date = new Date('2022-06-30T12:00:00.000000-03:00')
+    const day = String(faker.datatype.number({ min: 1, max: 28 })).padStart(2)
+    const year = faker.datatype.number({ min: 2000, max: 2100 })
+    survey.date = new Date(`${year}-06-${day}T12:00:00.000000-03:00`)
     makeSut(survey)
-    expect(screen.getByTestId('day')).toHaveTextContent('30')
+    expect(screen.getByTestId('day')).toHaveTextContent(`${day}`)
     expect(screen.getByTestId('month')).toHaveTextContent('jun')
-    expect(screen.getByTestId('year')).toHaveTextContent('2022')
+    expect(screen.getByTestId('year')).toHaveTextContent(`${year}`)
   })
 })
