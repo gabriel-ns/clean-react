d9ff6476a73cddbd7f87549b2454620cc1c8b6bb
Gabriel Santos <gaabrielns@gmail.com>
2022-07-07 11:49:26 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/survey-list/components/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item.spec.tsx
@@ -4,31 +4,35 @@ import { IconName } from '@/presentation/components'
 import { SurveyItem } from '@/presentation/pages/survey-list/components'
 import { render, screen } from '@testing-library/react'
 
+const makeSut = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={ survey }/>)
+}
+
 describe('SurveyItem component', () => {
   test('Should render with thumb up icon if question is answered', () => {
     const survey = mockSurveyModel()
     survey.didAnswer = true
-    render(<SurveyItem survey={ survey }/>)
+    makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
   })
 
   test('Should render with thumb down icon if question is answered', () => {
     const survey = mockSurveyModel()
     survey.didAnswer = false
-    render(<SurveyItem survey={ survey }/>)
+    makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
   })
 
   test('Should render with correct question', () => {
     const survey = mockSurveyModel()
-    render(<SurveyItem survey={ survey }/>)
+    makeSut(survey)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
   })
 
   test('Should render with correct date', () => {
     const survey = mockSurveyModel()
     survey.date = new Date('2022-06-30T12:00:00.000000-03:00')
-    render(<SurveyItem survey={ survey }/>)
+    makeSut(survey)
     expect(screen.getByTestId('day')).toHaveTextContent('30')
     expect(screen.getByTestId('month')).toHaveTextContent('jun')
     expect(screen.getByTestId('year')).toHaveTextContent('2022')
