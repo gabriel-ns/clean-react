854aae0e156d0ec08bffea9872887a5d4234f480
Gabriel Santos <gaabrielns@gmail.com>
2022-07-07 11:24:55 -0300

    feat: ensure SurveyItem renders with correct icon

--- src/presentation/pages/survey-list/components/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item.tsx
@@ -1,12 +1,17 @@
+import { SurveyModel } from '@/domain/models'
 import { Icon, IconName } from '@/presentation/components'
 import React from 'react'
 import Styles from './survey-item.scss'
 
-const SurveyItem: React.FC = () => {
+type Props = {
+  survey: SurveyModel
+}
+
+const SurveyItem: React.FC<Props> = ({ survey }) => {
   return (
-    <li className={Styles.surveyItemWrap}>
+  <li className={Styles.surveyItemWrap}>
     <div className={Styles.surveyContent}>
-      <Icon iconName={IconName.thumbDown} className={Styles.iconWrap}/>
+      <Icon iconName={survey.didAnswer ? IconName.thumbUp : IconName.thumbDown} className={Styles.iconWrap}/>
       <time>
         <span className={Styles.day}>22</span>
         <span className={Styles.month}>12</span>
