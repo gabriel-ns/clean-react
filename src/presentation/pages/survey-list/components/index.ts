b703d4fe92e22f3873b68ac518965ffc585d4af4
Gabriel Santos <gaabrielns@gmail.com>
2022-07-08 13:13:34 -0300

    feat: add SurveyContext

--- src/presentation/pages/survey-list/components/index.ts
+++ src/presentation/pages/survey-list/components/index.ts
@@ -1,2 +1,3 @@
 export { default as SurveyItemEmpty } from './item-empty/item-empty'
 export { default as SurveyItem } from './item/item'
+export { default as SurveyContext } from './context/context'
